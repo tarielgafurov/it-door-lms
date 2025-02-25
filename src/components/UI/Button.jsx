7fc8ed56a23c6ab030d0a05cf55396dbe29cc4cf
tarielgafurov <tarielgafurov618@gmail.com>
2025-02-25 14:10:30 +0300

    ui button finished

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+ import styled from 'styled-components'
+
+const StyledButton =styled.button`
+width:200px;
+height:50px;
+padding:12px;
+margin-top:20px;
+background-color:#116378;
+color:white;
+margin:auto;
+margin-top:10px;
+justify-content:center;
+display:flex;
+flex-direction:column;
+font-size:18px;
+border:none;
+border-radius:5px;
+cursor: pointer;
+&:hover{
+  background-color:#1b6476;
+}
+
+
+`
+
+
 
 const Button = (props) => {
-    
+  
   return (
-    <button>{props.children}</button>
+    <StyledButton>{props.children}</StyledButton>
   )
 }
 
