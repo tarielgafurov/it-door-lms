1560b61c8aab2734c206fde5329f59ab0938f74d
BamBook <bambook@MacBook-Pro-BamBook.local>
2025-02-26 23:24:06 +0600

    ui input finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { Input } from './components/UI/Input';
+
+
+
+
 
 
 function App() {
   
   return (
     <div className="App">
-      
+     <Input></Input>
+    <Input></Input>
     </div>
   );
 }
---
+++ src/components/UI/Input.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components"
+
+
+const InputStyled = styled.input`
+  width: 25%;
+  padding: 10px;
+  margin: 10px 0;
+  border:none;
+  margin:auto;
+  margin-top:1opx;
+  justify-content:center;
+  display:flex;
+  flex-direction:column;
+  border-bottom:1px solid gray;
+  font-size: 16px;
+  outline:none;
+  
+  &:focus {
+    border-bottom:2px solid #0056b3;
+    
+  }
+`;
+
+  export const Input=(props)=>{
+    return( 
+     < InputStyled placeholder={props.placeholder} type={props.text} value={props.value}>{props.children}</InputStyled>
+       
+    )
+  }
